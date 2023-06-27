d3f60d8cf6675a4525148e604c7dfb714662cd14
Victor Macedo <123992024+vctrmacedo@users.noreply.github.com>
2023-06-27 20:11:27 -0300

    Add files via upload

---
+++ bot Discord Frases inspiradoras- by Victor/bot.py
@@ -0,0 +1,32 @@
+import discord
+import json
+import random
+
+with open("json.json", encoding='utf-8') as json_data:
+   dados = json.load(json_data)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logou como {self.user.name}!')
+
+    async def on_message(self, message):
+        print(f'Mensagem de {message.author}: {message.content}')
+        if message.content == '$eae':
+            await message.channel.send(f'Eae {message.author.name}, beleza?')
+        elif message.content == '$piroca':
+            await message.channel.send('rolas de 13cm ou 20cm?')
+        elif message.content == '$frase':
+            frase_aleatoria = random.choice(dados)
+            autor = frase_aleatoria['autor']
+            frase = frase_aleatoria['frase']
+            await message.channel.send(f'{frase} - {autor}')
+
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run('SUA KEY') ##Não esqueça de alterar sua chave aqui.
+
+
+
